4c40796a94343f5f0593fc4bb51b6b5c10638a80
cnerylozada <cnerylozada@gmail.com>
2026-01-24 10:16:41 -0500

    feat: inserting rows in batch mode

--- resources/challenge/insertBasicEntities.ts
+++ resources/challenge/insertBasicEntities.ts
@@ -1,21 +1,33 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient, PutRequest } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  PutCommand,
+  BatchWriteCommand,
+} from "@aws-sdk/lib-dynamodb";
 
 export const mainHandler = async () => {
   const client = new DynamoDBClient();
-  const docClient = DynamoDBDocumentClient.from(client);
+  const documentClient = DynamoDBDocumentClient.from(client);
 
-  const HEALTH_SYSTEM_TABLE = process.env.HEALTH_SYSTEM_TABLE;
-  const command = new PutCommand({
-    TableName: HEALTH_SYSTEM_TABLE,
-    Item: {
-      PK: `COUNTRY#002`,
-      SK: "METADATA",
-      name: `Chile`,
-      code: `CL`,
+  const healthSystemTable = process.env.HEALTH_SYSTEM_TABLE;
+
+  const hospitalList = [
+    { PK: "COUNTRY#001", SK: "HOSPITAL#001", id: "001", name: "Hospital 001" },
+    { PK: "COUNTRY#001", SK: "HOSPITAL#002", id: "002", name: "Hospital 002" },
+    { PK: "COUNTRY#002", SK: "HOSPITAL#003", id: "003", name: "Hospital 003" },
+  ];
+  const putRequestList = hospitalList.map((_) => ({
+    PutRequest: {
+      Item: _,
+    },
+  }));
+
+  const command = new BatchWriteCommand({
+    RequestItems: {
+      [healthSystemTable!]: putRequestList,
     },
   });
 
-  const response = await docClient.send(command);
+  const response = await documentClient.send(command);
   return { response };
 };
